df78ce552055916177273dd56cd01a2c04d43e84
nafisFuad <30768026+fuadmmnf@users.noreply.github.com>
2018-07-07 10:44:22 +0600

    improved db

--- src/Robots.js
+++ src/Robots.js
@@ -58,5 +58,113 @@ export const robots =[
 		name : 'Saikat Sen',
 		roll : '0910',
 		email: 'bsse0910@iit.du.ac.bd'
+	},
+	{
+		id : 11 ,
+		name : 'Laila Afroz Labonno',
+		roll : '0911',
+		email: 'bsse0911@iit.du.ac.bd'
+	},
+	{
+		id : 12 ,
+		name : 'Md Saleh Ibtasham',
+		roll : '0912',
+		email: 'bsse0912@iit.du.ac.bd'
+	},
+	{
+		id : 14 ,
+		name : 'Noshin Tahsin',
+		roll : '0914',
+		email: 'bsse0914@iit.du.ac.bd'
+	},
+	{
+		id : 16 ,
+		name : 'Md Samsarabbi',
+		roll : '0916',
+		email: 'bsse0916@iit.du.ac.bd'
+	},
+	{
+		id : 17 ,
+		name : 'Amran Hossain',
+		roll : '0917',
+		email: 'bsse0917@iit.du.ac.bd'
+	},
+	{
+		id : 18 ,
+		name : 'Saiba Alam Pritul ',
+		roll : '0918',
+		email: 'bsse0918@iit.du.ac.bd'
+	},
+	{
+		id : 19 ,
+		name : 'Pritom Kumar Das',
+		roll : '0919',
+		email: 'bsse0919@iit.du.ac.bd'
+	},
+	{
+		id : 20 ,
+		name : 'Mridha Md Nafis Fuad',
+		roll : '0920',
+		email: 'bsse0920@iit.du.ac.bd'
+	},
+	{
+		id : 21 ,
+		name : 'Fahmid Ahmed',
+		roll : '0921',
+		email: 'bsse0921@iit.du.ac.bd'
+	},
+	{
+		id : 22 ,
+		name : 'Ishita Sur Apan',
+		roll : '0922',
+		email: 'bsse0922@iit.du.ac.bd'
+	},
+	{
+		id : 23 ,
+		name : 'Md Ezazul Haque',
+		roll : '0923',
+		email: 'bsse0923@iit.du.ac.bd'
+	},
+	{
+		id : 24 ,
+		name : 'Saif Kamal Chowdhury',
+		roll : '0924',
+		email: 'bsse0924@iit.du.ac.bd'
+	},
+	{
+		id : 25 ,
+		name : 'Anika Tabassum',
+		roll : '0925',
+		email: 'bsse0925@iit.du.ac.bd'
+	},
+	{
+		id : 26 ,
+		name : 'ATM Fazlay Rabbi',
+		roll : '0926',
+		email: 'bsse0926@iit.du.ac.bd'
+	},
+	{
+		id : 27 ,
+		name : 'Ishrat Jahan Emu',
+		roll : '0927',
+		email: 'bsse0927@iit.du.ac.bd'
+	},
+	{
+		id : 30 ,
+		name : 'Nazifa Tasnim HIa',
+		roll : '0930',
+		email: 'bsse0930@iit.du.ac.bd'
+	},
+	{
+		id : 31 ,
+		name : 'Miran Al Mehrab',
+		roll : '0931',
+		email: 'bsse0931@iit.du.ac.bd'
+	},
+	{
+		id : 33 ,
+		name : 'Md Rezowan Talukder',
+		roll : '0933',
+		email: 'bsse0933@iit.du.ac.bd'
 	}
 ]
