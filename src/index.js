f87b9b5a6a5b9b2a5afdb77fd5163e22478efded
fuadmmnf <bsse0920@iit.du.ac.bd>
2018-07-07 10:40:30 +0600

    complete db

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createBrowserHistory } from "history";
+import { Router, Route, Switch } from "react-router-dom";
 
-import registerServiceWorker from './registerServiceWorker';
+import "assets/css/material-dashboard-react.css?v=1.3.0";
+
+import indexRoutes from "routes/index.jsx";
+
+const hist = createBrowserHistory();
 
 ReactDOM.render(
-	<div>
-		<App/>
-	</div>
-	, document.getElementById('root'));
-registerServiceWorker();
+  <Router history={hist}>
+    <Switch>
+      {indexRoutes.map((prop, key) => {
+        return <Route path={prop.path} component={prop.component} key={key} />;
+      })}
+    </Switch>
+  </Router>,
+  document.getElementById("root")
+);
