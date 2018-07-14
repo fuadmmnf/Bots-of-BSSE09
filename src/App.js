c11a713fbc99dbcc5f829e56928a098ebd79067a
fuadmmnf <30768026+fuadmmnf@users.noreply.github.com>
2018-07-15 00:47:32 +0600

    particle

--- src/App.js
+++ src/App.js
@@ -3,8 +3,36 @@ import React,{Component} from 'react';
 import RobotList from './RobotList';
 import {robots} from './Robots';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll';
 import './App.css';
 import 'tachyons';
+import Particles from 'react-particles-js';
+
+
+
+const particleProperty=
+{
+	particles: {
+		number:{
+			value: 150,
+			density:{
+				enable: true,
+				value_area: 800
+			}
+
+		}
+    },
+    "detect_on":"canvas",
+    "interactivity":{
+    	"events":
+    	{
+	    	"onhover":{
+	    		"enable": true,
+	    		"mode": 'repulse'
+	    	}
+	    }
+    }
+}
 
 
 
@@ -26,19 +54,43 @@ class App extends Component {
 		//console.log(filteredRobots);
 	}
 
+/*
+	componentDidMount()
+	{
+		fetch("https://jsonplaceholder.typicode.com/users")
+		.then(response => response.json())
+		.then(users => this.setState({robots: users}));
+	}
+
+*/
+
 	render(){
 
 		const filteredRobots = this.state.robots.filter(robots => 
 			{return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase())});
 
-		return(
-			<div className='tc'>
-			<h1>RoboFriends</h1>
-			<SearchBox searchChange={this.onSearchChange}/>
-			<RobotList robots={filteredRobots}/>
-			</div>
-		);
+		if(this.state.robots.length === 0)
+		{
+			return <h1>Loading</h1>
+		}
+		else
+		{
+			return(
+					<div className='tc'>
+					<h1>RoboFriends</h1>
+					 <Particles className='particles'
+	             	 params={particleProperty} />
+					<SearchBox searchChange={this.onSearchChange}/>
+					<Scroll>
+						<RobotList robots={filteredRobots}/>
+					</Scroll>
+					</div>
+			);
+		}
 	}
+
+
+
 }
 
 export default App;
